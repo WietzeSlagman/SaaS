c90839bc17a02c8dbc269dab892382538f05a106
MangoDream1 <axel.verheul@gmail.com>
2018-04-06 19:46:55 +0200

    Location updating works for drone

--- AgentModel/DroneAPI.js
+++ AgentModel/DroneAPI.js
@@ -2,6 +2,9 @@ const sumo = require("node-sumo")
 const dbinterface = require("../BigChainDBInterface/interface")
 const chalk = require('chalk');
 
+const WAITTIME = 100
+const STARTUPTIME = 100
+
 class Drone {
     constructor(id, init_location) {
         this.drone = sumo.createClient()
@@ -29,7 +32,9 @@ class Drone {
         })
 
         // fixed speed 50
-        this.movement = this.createMovementControls(50)
+        this.speed = 50
+
+        this.movement = this.createMovementControls()
     }
 
     getBatteryLifePromise() {
@@ -57,63 +62,115 @@ class Drone {
         })
     }
 
-    createMovementControls(speed) {
+    _setLocation(distance, multiplier) {
+        switch (this.facing) {
+            case "N":
+                this.location.x -= distance * multiplier
+                break;
+            case "E":
+                this.location.y += distance * multiplier
+                break;
+            case "S":
+                this.location.x += distance * multiplier
+                break;
+            case "W":
+                this.location.y -= distance * multiplier
+                break;
+            default:
+                console.log(chalk.red("IMPOSSIBLE FACING"));
+        }
+
+        return this.location
+    }
+
+    _forward(time) {
+        return new Promise(function(resolve, reject) {
+            this.drone.forward(this.speed)
+
+            setTimeout(() => {
+                this.drone.stop()
+                this._setLocation(time, 1)
+
+                setTimeout(() => {
+                    resolve()
+                }, WAITTIME)
+
+            }, time + STARTUPTIME)
+        }.bind(this));
+    }
+
+    _backward(time) {
+        return new Promise(function(resolve, reject) {
+            this.drone.backward(this.speed)
+
+            setTimeout(() => {
+                this.drone.stop()
+                this._setLocation(time, -1)
+
+                setTimeout(() => {
+                    resolve()
+                }, WAITTIME)
+
+            }, time + STARTUPTIME)
+        }.bind(this));
+    }
+
+    _right() {
+        return new Promise(function(resolve, reject) {
+            var current = this.directions.indexOf(this.facing)
+            var next = current + 1;
+
+            // if greater than length then 0
+            if (next >= this.directions.length) {
+                next = 0
+            }
+
+            this.facing = this.directions[next]
+
+            this.drone.right(this.speed)
+
+            setTimeout(() => {
+                this.drone.stop()
+
+                setTimeout(() => {
+                    resolve()
+                }, WAITTIME)
+            }, 350) // magic number; almost right
+
+        }.bind(this));
+    }
+
+    _left() {
+        return new Promise(function(resolve, reject) {
+            var current = this.directions.indexOf(this.facing)
+            var next = current -= 1
+
+            // if less than 0 then 0
+            if (next < 0) {
+                next = 0
+            }
+
+            this.facing = this.directions[next]
+            this.drone.left(this.speed)
+
+            setTimeout(() => {
+                this.drone.stop()
+
+                setTimeout(() => {
+                    resolve()
+                }, WAITTIME)
+            }, 350) // magic number; almost left
+        }.bind(this));
+    }
+
+    createMovementControls() {
         return new Promise(function(resolve, reject) {
             this.connected.then(() => {
                 resolve({
-                    forward: () => {
-                        this.drone.forward(speed)
-                    },
-                    backward: () => {
-                        this.drone.backward(speed)
-                    },
-                    right: () => {
-                        return new Promise(function(resolve, reject) {
-                            var current = this.directions.indexOf(this.facing)
-                            var next = current + 1;
-
-                            // if greater than length then 0
-                            if (next >= this.directions.length) {
-                                next = 0
-                            }
-
-                            this.facing = this.directions[next]
-
-                            this.drone.right(speed)
-
-                            setTimeout(() => {
-                                this.drone.stop()
-
-                                setTimeout(() => {
-                                    resolve()
-                                }, 100)
-                            }, 350) // magic number; almost left
-
-                        }.bind(this));
-                    },
-                    left: () => {
-                        return new Promise(function(resolve, reject) {
-                            var current = this.directions.indexOf(this.facing)
-                            var next = current -= 1
-
-                            // if less than 0 then 0
-                            if (next < 0) {
-                                next = 0
-                            }
-
-                            this.facing = this.directions[next]
-                            this.drone.left(speed)
-
-                            setTimeout(() => {
-                                this.drone.stop()
-
-                                setTimeout(() => {
-                                    resolve()
-                                }, 100)
-                            }, 350) // magic number; almost left
-
-                        });
-                    },
+                    forward: this._forward.bind(this),
+                    backward: this._backward.bind(this),
+                    right: this._right.bind(this),
+                    left: this._left.bind(this),
                     stop: () => { this.drone.stop() },
                 })
             })
@@ -126,19 +183,23 @@ var d  = new Drone("test", {x: 0, y:0})
 d.movement.then(movement => {
     movement.right().then(() => {
         console.log(d.facing);
-        movement.right().then(() => {
+        console.log(d.location);
+
+        movement.forward(1000).then(() => {
             console.log(d.facing);
+            console.log(d.location);
 
             movement.right().then(() => {
                 console.log(d.facing);
+                console.log(d.location);
 
-                movement.right().then(() => {
+                movement.forward(1000).then(() => {
+                    console.log(d.location);
                     console.log(d.facing);
                 })
-            // })
+            })
         })
     });
-
 
     // console.log("facing", d.facing);
     //
