06e7b5a4245d1285d4bdcc12203ed112651545ad
Wietze Slagman <wietzeslagman@gmail.com>
2018-04-06 18:13:31 +0200

    add image recognition python + distance calculation

--- webcam.py
+++ webcam.py
@@ -1,107 +1,62 @@
 from collections import deque
 import numpy as np
+import sys
 import imutils
 import cv2
 
-def detect_iets(frame):
-    # define the lower and upper boundaries of the "green"
-    # ball in the HSV color space, then initialize the
-    # list of tracked points
-    greenLower = (29, 86, 6)
-    greenUpper = (64, 255, 255)
-    pts = deque(maxlen=100)
-
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
+def detectObject(frame, lower, upper):
     frame = imutils.resize(frame, width=600)
-    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
+
     hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
+    mask = cv2.inRange(hsv, lower, upper)
     mask = cv2.erode(mask, None, iterations=2)
     mask = cv2.dilate(mask, None, iterations=2)
 
     cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
     center = None
 
-    # only proceed if at least one contour was found
     if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
-
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-            (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
-
-            # update the points queue
-    pts.appendleft(center)
+        return cnts[0]
+    return None
 
 
-cv2.namedWindow("preview")
-vc = cv2.VideoCapture(0)
+def calculateDistance(cnts):
+    rectSize = cv2.minAreaRect(cnts)
 
-if vc.isOpened(): # try to get the first frame
-    rval, frame = vc.read()
-else:
-    rval = False
+    return (rectSize * KNOWN_DISTANCE) / KNOWN_WIDTH
 
-while rval:
-    cv2.imshow("preview", frame)
-    rval, frame = vc.read()
-    #detect_iets(frame)
-    greenLower = (120, 0, 0)
-    greenUpper = (255, 130, 130)
-    pts = deque(maxlen=100)
+def main():
+    #redLower = (170, 200, 200)
+    #redUpper = (179, 255, 255)
+    lower = (29, 86, 6)
+    upper = (64, 255, 255)
 
-    # resize the frame, blur it, and convert it to the HSV
-    # color space
-    frame = imutils.resize(frame, width=600)
-    # blurred = cv2.GaussianBlur(frame, (11, 11), 0)
-    hsv = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
+    #frame = sys.argv[1]
 
-    # construct a mask for the color "green", then perform
-    # a series of dilations and erosions to remove any small
-    # blobs left in the mask
-    mask = cv2.inRange(hsv, greenLower, greenUpper)
-    mask = cv2.erode(mask, None, iterations=2)
-    mask = cv2.dilate(mask, None, iterations=2)
+    cv2.namedWindow("preview")
+    vc = cv2.VideoCapture(0)
 
-    cnts = cv2.findContours(mask.copy(), cv2.RETR_EXTERNAL, cv2.CHAIN_APPROX_SIMPLE)[-2]
-    center = None
+    if vc.isOpened(): # try to get the first frame
+        rval, frame = vc.read()
+    else:
+        rval = False
 
-    # only proceed if at least one contour was found
-    if len(cnts) > 0:
-        # find the largest contour in the mask, then use
-        # it to compute the minimum enclosing circle and
-        # centroid
-        c = max(cnts, key=cv2.contourArea)
-        ((x, y), radius) = cv2.minEnclosingCircle(c)
-        M = cv2.moments(c)
-        center = (int(M["m10"] / M["m00"]), int(M["m01"] / M["m00"]))
+    while rval:
+        cv2.imshow("preview", frame)
+        rval, frame = vc.read()
+        detectObject(frame, lower, upper)
+        key = cv2.waitKey(20)
+        if key == 27: # exit on ESC
+            break
+    cv2.destroyWindow("preview")
 
-        # only proceed if the radius meets a minimum size
-        if radius > 10:
-            # draw the circle and centroid on the frame,
-            # then update the list of tracked points
-            cv2.circle(frame, (int(x), int(y)), int(radius),
-            (0, 255, 255), 2)
-            cv2.circle(frame, center, 5, (0, 0, 255), -1)
+    # contours = detectObject(frame, lower, upper)
+    #
+    # if contours:
+    #     return calculateDistance(contours)
+    # else:
+    #     return None
 
-            # update the points queue
-    pts.appendleft(center)
-
-    key = cv2.waitKey(20)
-    if key == 27: # exit on ESC
-        break
+if __name__ == '__main__':
+    main()
