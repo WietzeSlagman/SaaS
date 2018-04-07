d4c39ea0a2b65feb1434bb88b4cd955c59175914
Max Bos <max@trytabular.com>
2018-04-07 20:44:58 +0200

    Added prototypes; drone info percentage

--- Frontend/src/scenes/Mission/index.jsx
+++ Frontend/src/scenes/Mission/index.jsx
@@ -16,48 +16,56 @@ const DRONES = [
     type: 'VIDEO',
     battery: '30',
     coords: { x: 0, y: 0 },
+    percentage: 10
   },
   {
     id: '12312093j2130ijdsjkjsd0129j231lk21jl132kj31l2',
     type: 'VIDEO',
     battery: '40',
     coords: { x: 2, y: 5 },
+    percentage: 20
   },
   {
     id: '12312312y3iosjdosdajsadsausad7132123hoi132hoi',
     type: 'VIDEO',
     battery: '90',
     coords: { x: 10, y: 30 },
+    percentage: 40
   },
   {
-    id: '4',
+    id: '124f8su9sad8213jj1k23lj123kl123j98123j123123',
     type: 'WORKER',
     battery: '12',
     coords: { x: 1, y: 9 },
+    percentage: 60
   },
   {
-    id: '5',
+    id: '1231293012j03k1l23j12o3129831j23k12n3lk12312',
     type: 'WORKER',
     battery: '41',
     coords: { x: 9, y: 4 },
+    percentage: 25
   },
   {
-    id: '6',
+    id: '123123jo1i2n3kl12n312983y123k12n3n123123123123',
     type: 'WORKER',
     battery: '30',
     coords: { x: 11, y: 3 },
+    percentage: 63
   },
   {
-    id: '7',
+    id: '123oh1238123hio12312klh3123y12hi31n2k3h1203123',
     type: 'WORKER',
     battery: '30',
     coords: { x: 88, y: 43 },
+    percentage: 74
   },
   {
-    id: '8',
+    id: '123j12931u2ui12nk3n12o3ih12io3kn123n12lk31o2kn',
     type: 'WORKER',
     battery: '30',
     coords: { x: 19, y: 14 },
+    percentage: 56
   },
 ];
 
