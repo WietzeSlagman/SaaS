130fe9590f4033a0f4c7efa399a6e130d5cf425a
regenworm <a.khawalid@gmail.com>
2018-04-07 14:15:40 +0200

    ORMInterface

---
+++ Backend/BigchainDB/ORMInterface.js
@@ -0,0 +1,70 @@
+const bdbOrm        = require('bigchaindb-orm');
+const DB_ENDPOINT   = 'http://192.168.169.44:9984/api/v1/';
+const driver        = require('bigchaindb-driver')
+
+
+class ORMInterface {
+    constructor() {
+        // console.log(bdbOrm.default())
+        this.bdbOrm = new bdbOrm.default(DB_ENDPOINT)
+
+        this.bdbOrm.define("droneModel", {
+            id: String,
+            location: String,
+            action: String,
+
+            object_detected: Boolean,
+            battery: Number,   
+            cost: Number,
+            type: String
+        });
+
+
+        this.bdbOrm.define("gridModel", {
+            type: String,
+            location: String,
+            drones: Object,
+            grid: Object,
+            name: String
+        });
+    }
+
+    createKeyPair() {
+        return new driver.Ed25519Keypair()
+    }
+    
+    create(keypair, data, type) {
+        return this.bdbOrm[type].create({
+            keypair: id,
+            data: data
+        }).then(object => {
+            return object;
+        }).catch((e) => {
+            return e;
+        });
+    }
+
+    retrieve(id, type) {
+        return this.bdbOrm[type].retrieve(id).then(objects => {
+            return objects;
+        }).catch((e) => {
+            return e;
+        });
+    }
+
+    append(id, keypair, data, type) {
+        return this.bdbOrm[type].append({
+            toPublicKey: id,
+            keypair: keypair,
+            data: data
+        }).then(object => {
+            return object;
+        }).catch((e) => {
+            return e;
+        });
+    }
+
+}
+
+module.exports = new ORMInterface()
+// export default const bla = new BigChainDBInterface
--- Backend/Endpoints/Mission.js
+++ Backend/Endpoints/Mission.js
@@ -1,7 +1,9 @@
-var BigchainDB      = require('../BigchainDB/Interface.js')
-var fetch 			= require('node-fetch');
-var fs 				= require('fs');
-var cors 			= require('cors')
+// const BigchainDB    = require('../BigchainDB/Interface.js');
+const BigchainDB	= require('../BigchainDB/ORMInterface');
+const fetch 		= require('node-fetch');
+const fs 			= require('fs');
+const cors 			= require('cors');
+
 
 // express
 const express 		= require('express');
@@ -12,16 +14,16 @@ const app 			= express();
 // printing
 var chalk 			= require('chalk');
 
-app.use(bodyParser.json())
-app.use(cors())
+app.use(bodyParser.json());
+app.use(cors());
 
 
 app.post('/api/createMission', (req, res) => {
     return new Promise((resolve, reject) => {
-
+    	// ORMInterface();
     })
 })
 
 app.listen(6868, () => {
-    console.log('Mission creation is a gogo')
+    console.log('Mission creation is a gogo');
 })
