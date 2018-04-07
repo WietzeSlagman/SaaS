4a2d9c8cfd0e0d1b8ef5f7494e7f1d2d860cf3fb
Haischel <Haischel@me.com>
2018-04-07 11:42:49 +0200

    logo and category animations

--- Frontend/src/components/Logo/index.jsx
+++ Frontend/src/components/Logo/index.jsx
@@ -9,7 +9,7 @@ const propTypes = {
 };
 
 const defaultProps = {
-  width: 36,
+  width: 46,
   showName: false,
   showSlogan: false,
 };
@@ -19,11 +19,11 @@ class Logo extends React.Component {
     const { width, showName, showSlogan } = this.props;
     return (
       <div>
-        <svg xmlns="http://www.w3.org/2000/svg" width={width} viewBox="0 0 107.18 93.45">
+        <svg xmlns="http://www.w3.org/2000/svg" width={width} viewBox="0 0 107.18 93.45" className="bayleyssvg">
           <title>Asset 1</title>
           <g id="Layer_2" data-name="Layer 2">
           <g id="Eblems">
-          <path class="cls-1" d="M103.72,90.76,3.47,90.87,28,48.32,53.62,4,79.21,48.32Zm0,0L40.12,27.17m-23.2,40.5,22.95,23,32.05-32" />
+          <path className="cls-1 path" d="M103.72,90.76,3.47,90.87,28,48.32,53.62,4,79.21,48.32Zm0,0L40.12,27.17m-23.2,40.5,22.95,23,32.05-32" id="bayleys"/>
           </g>
           </g>
         </svg>
