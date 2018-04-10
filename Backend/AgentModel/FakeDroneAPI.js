ff9d2bccb4f330a802bb324c75a24b9b967d9169
MangoDream1 <axel.verheul@gmail.com>
2018-04-10 18:06:33 +0200

    not instant move

--- Backend/AgentModel/FakeDroneAPI.js
+++ Backend/AgentModel/FakeDroneAPI.js
@@ -8,8 +8,32 @@ class FakeDrone extends Drone {
     }
 
     goto(location) {
+        if (JSON.stringify(location) === JSON.stringify(this.location)) {
+            return new Promise(function(resolve, reject) {
+                resolve()
+            });
+        }
+
+        if (Math.random() > 0.5) {
+            if (location.x > this.location.x) {
+                location.x = this.location.x + 5
+                location.y = this.location.y
+            } else {
+                location.x = this.location.x - 5
+                location.y = this.location.y
+            }
+        } else {
+            if (location.y > this.location.y) {
+                location.y = this.location.y + 5
+                location.x = this.location.x
+            } else {
+                location.y = this.location.y - 5
+                location.x = this.location.x
+            }
+        }
+
         return new Promise(function(resolve, reject) {
-            console.log(chalk.blue("Going to "), location);
+            console.log(chalk.blue("Going to "), this.location);
             this.location = location
             this.setStateBigchain()
             resolve()
