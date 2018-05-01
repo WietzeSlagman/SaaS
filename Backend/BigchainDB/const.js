d1a40c36718f2217b2b4c62d496c59b090201eae
MangoDream1 <axel.verheul@gmail.com>
2018-05-01 17:32:34 +0200

    made docker compliant

--- Backend/BigchainDB/const.js
+++ Backend/BigchainDB/const.js
@@ -4,8 +4,8 @@
 // }
 
 const DB_ENDPOINT = {
-    db: 'http://127.0.0.1:9984/api/v1/',
-    ws: 'ws://127.0.0.1:9985/api/v1/streams/valid_transactions',
+    db: 'http://bigchaindb:9984/api/v1/',
+    ws: 'ws://bigchaindb:9985/api/v1/streams/valid_transactions',
 }
 
 module.exports = DB_ENDPOINT
